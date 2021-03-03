2ffa6af4cd6afb98b437785454db30478b46afa0
陈怀剑 <76472532+chen-huai-jian@users.noreply.github.com>
2021-03-03 10:29:27 +0800

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,12 @@
+//bigint
+// 高精度的时间戳， MYSQL id 大数据
+const theBiggestInt = 9007199254740991n; 
+// console.log(typeof theBiggestInt);
+const alsoHuge = BigInt(9007199254740991);
+
+const b1 = BigInt(Number.MAX_SAFE_INTEGER);
+console.log(b1);
+console.log(BigInt(Number.MIN_SAFE_INTEGER));
+
+const maxPlusOne = b1 + 1n;
+console.log(maxPlusOne);
