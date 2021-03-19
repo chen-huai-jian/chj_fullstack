a1d4dc5e527394908852963a2e8f22efd8408532
陈怀剑 <76472532+chen-huai-jian@users.noreply.github.com>
2021-03-19 23:10:18 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('message', msg => {
-      console.log(msg);
-      io.emit('message', msg)
-  })
-});
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+const koa = require('koa')
+const mount = require('koa-mount')
+const serve=require('koa-static')  // 将static所有的静态资源调用, 不需要一个一个映入url
+//静态文件static ->cdn 静态服务器 
+const fs =require('fs')
+const app = new koa();
+app.use(serve(__dirname + '/images'));
+app.use(serve(__dirname + '/static/'));
+console.log(__dirname+'/static/');
+app.use(
+    mount('/',ctx =>{
+       ctx.body = fs.readFileSync(__dirname + '/max_count.html', 'utf-8');
+    })
+)
+app.listen(1234)
--- readme.md
+++ readme.md
@@ -1,23 +1,11 @@
-# 设计组件学习---
+- flex
+   分割线打理的专业度
+   flex 布局
+   display:inline-flex  子元素有flex 接管
+   .divider border-right
+   min-width  用法
 
-不是切页面，写组件， -- 设计组件
 
-把组件发布到npm去，省市选择，
 
-- 通过一个原生的js
-- 分析出react 的组件设计的点
-
-    1. 有一些状态， hasWinner -> Game
-    chess: 1  MVVM
-    pattern  二维数组
-    数据修改 ， 操作dom   build（） 在执行一次
-    MVVM  来做   数据状态的改变以界面的响应式，结合起来
-    onClick  -> 修改数据  -> 更改界面 
-
-    react -> 响应式  
-
-    2. 艳杰 悔棋  下棋的历史记录
-        良哥  三维
-        怎么设计数据状态
-        设计组件
-        组件三个   GAMe  Board Square
+- 扒小米的官网
+   
