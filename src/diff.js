41e44654500cf2a6311d8d097a6cdbd5ec4e78f7
陈怀剑 <76472532+chen-huai-jian@users.noreply.github.com>
2021-04-06 09:57:08 +0800

    Add files via upload

---
+++ src/diff.js
@@ -0,0 +1,17 @@
+const diff = (oldTree, newTree) => {
+
+};
+
+const walk = () => {
+   
+}
+// 比对属性的
+const diffAttr = () => {
+   
+}
+// 比较字结点
+const diffChildren = () => {
+   
+}
+
+export default diff;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
+import { createElement, render, renderDOM } from './element';
+import {diff} from './diff'
+// DOM  ->  VIrtualDOM
+// 内存之中创建  模拟真实dom的层次， 节点，属性， 子节点
+// 树 递归的概念  最后节点是文本节点时， 就退出
+// 相同的操作 1. 创建结点  2. 设置属性 3. 子节点
 
-/* redux */
-import { Provider } from 'react-redux';
-import store from './store/index';
+{
+   /* <ul class="list">
+   <li class="item">白敬亭</li>
+   <li class="item">婷婷</li>
+</ul>; */
+}
+// DOM 树的表达转换形式  递归  每一部分都有三个参数（标签，属性，子节点）
+let virtualDOM = createElement(
+   'ul',
+   {
+      class: 'list',
+   },
+   [
+      createElement(
+         'li',
+         {
+            class: 'item',
+         },
+         ['白敬亭']
+      ),
+      createElement(
+         'li',
+         {
+            class: 'item',
+         },
+         ['婷婷']
+      ),
+   ]
+);
 
-ReactDOM.render(<Provider store={store}>
-	<HashRouter>
-		<App />
-	</HashRouter>
-</Provider>, document.getElementById('root')); 
+// console.log(virtualDOM);
+let el = render(virtualDOM);
+console.log(el);
+
+let virtualDOM2 = createElement(
+   'ul',
+   {
+      class: 'list-group',
+   },
+   [
+      createElement(
+         'li',
+         {
+            class: 'item active',
+         },
+         ['七里香']
+      ),
+      createElement(
+         'li',
+         {
+            class: 'item active',
+         },
+         ['七千年以后']
+      ),
+      createElement(
+         'li',
+         {
+            class: 'item active',
+         },
+         ['菊花台']
+      ),
+   ]
+);
+
+renderDOM(el, document.getElementById('root'));
+//补丁
+let patches = diff(virtualDOM, virtualDOM2);
+console.log(patches);
